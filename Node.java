90d03ed85277575fa069ccdf4e2b0e32879a926d
IvanInfinity <ivan.nicholas.williams@gmail.com>
2013-10-31 21:31:10 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,42 @@
+public class Node
+{
+private int myInfo;
+private Node next;
+//private Node previous;  // Used for double-LinkedLists
+    Node() { 
+        myInfo = 0;
+        next = null; 
+    }
+    
+   
+     
+    Node(int i)
+    {
+        myInfo = i;
+        next = null;
+    }
+   
+   
+    
+    public int getInfo()
+    {
+        return myInfo;   
+    }
+    
+    public void setInfo(int x)
+    {
+        myInfo = x;   
+    } 
+    
+   
+    
+    public Node getNext()
+    {
+        return next;   
+    }    
+    
+    public void setNext(Node m)
+    {
+        next = m;   
+    }
+}
